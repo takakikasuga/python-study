8b78168be102ec4ec2cb9b2ebea0651d8bc29b82
takakikasuga <taka.engineer.works@gmail.com>
2024-01-05 21:48:51 +0900

    数値型

---
+++ integer.py
@@ -0,0 +1,19 @@
+import math
+
+def get_integer():
+  print(2 + 2)
+  print(5 - 2)
+  print(50 - 2 * 3)
+  print((50 - 2) * 3)
+  print(8 / 5)
+  print(8 % 5)
+  print(17 / 3)
+  print(17 // 3)
+  print(5 ** 2)
+  print(round(3.141592, 2))
+  print(round(3.146592, 2))
+  result1 = math.sqrt(25)
+  print(result1)
+  result2 = math.log10(10)
+  print(result2)
+  print(help(math))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import variable
 import _print
+import integer
 
 print("Hello World!")
 variable.get_variable()
 _print.get_print()
+integer.get_integer()
