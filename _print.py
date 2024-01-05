aa33e18cbd4e9ac7a03a8e3700e8e809d8359409
takakikasuga <taka.engineer.works@gmail.com>
2024-01-05 21:40:10 +0900

    printで出力

---
+++ _print.py
@@ -0,0 +1,4 @@
+def get_print():
+  print("Hi!!")
+  print("Hi!!", "Mike", sep=",", end=".\n")
+  print("Hi!!", "Mike", sep=",", end=".\n")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import variable
+import _print
 
 print("Hello World!")
 variable.get_variable()
+_print.get_print()
