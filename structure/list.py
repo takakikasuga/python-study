cc3cb727633e4d0c2d101d6e48ba1d8c55cc64eb
takakikasuga <taka.engineer.works@gmail.com>
2024-01-06 23:19:11 +0900

    リストタイプ

---
+++ structure/list.py
@@ -0,0 +1,26 @@
+def get_list():
+    l = [1, 20, 4, 50, 2, 1, 2]
+    print(l)
+    print(l[0])
+    print(l[-1])
+    print(l[-2])
+    print(l[0:2])
+    print(l[:2])
+    print(l[2:5])
+    print(l[2:])
+    print(l[:])
+    print(len(l))
+    print(type(l))
+    print(list("abcdefg"))
+    n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(n[::2])
+    print(n[::-1])
+    a = ["a", "b", "c"]
+    n = [1, 2, 3]
+    x = [a, n]
+    print(x)
+    print(x[0])
+    print(x[1])
+    print(x[0][1])
+    print(x[1][2])
+
---
+++ structure/main.py
@@ -0,0 +1,3 @@
+import list
+
+list.get_list()
