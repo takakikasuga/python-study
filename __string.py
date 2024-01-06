dcd6e02ff35494dfbe5c3264e475b18a80abcda2
takakikasuga <taka.engineer.works@gmail.com>
2024-01-06 22:59:10 +0900

    文字列のインデックスとスライス

--- __string.py
+++ __string.py
@@ -1,22 +1,35 @@
 def get_string():
-  s1 = "test"
-  print(s1)
-  print("I don't know")
-  print('I don\'t know')
-  print('Hello \nHow are you?')
-  print(r'C:\name\name')
-  print('##################')
-  print("""\
-  Line1
-  Line2
-  Line3\
-  """)
-  print('##################')
-  print('Hi!' * 3 + 'Mike.')
-  print('Py' + 'thon')
-  print('Py''thon')
-  s2 = ("aaaaaaaaaaaaaaaa""bbbbbbbbbbb")
-  print(s2)
-  s3 = "aaaaaaaaaaaaaaaa"\
-       "bbbbbbbbbbb"
-  print(s3)
+     s1 = "test"
+     print(s1)
+     print("I don't know")
+     print('I don\'t know')
+     print('Hello \nHow are you?')
+     print(r'C:\name\name')
+     print('##################')
+     print("""\
+     Line1
+     Line2
+     Line3\
+     """)
+     print('##################')
+     print('Hi!' * 3 + 'Mike.')
+     print('Py' + 'thon')
+     print('Py''thon')
+     s2 = ("aaaaaaaaaaaaaaaa""bbbbbbbbbbb")
+     print(s2)
+     s3 = "aaaaaaaaaaaaaaaa"\
+          "bbbbbbbbbbb"
+     print(s3)
+     word = 'python'
+     print(word[0])
+     print(word[1])
+     print(word[-1])
+     print(word[0:2])
+     print(word[:2])
+     print(word[2:5])
+     print(word[2:])
+     word = "j" + word[1:]
+     print(word)
+     print(word[:])
+     n = len(word)
+     print(n)
