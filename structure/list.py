ec90556e45e6e4b672433dbd6bc75f778d190807
takakikasuga <taka.engineer.works@gmail.com>
2024-01-07 21:57:09 +0900

    リストのメソッド

--- structure/list.py
+++ structure/list.py
@@ -60,3 +60,28 @@ def get_list():
     y = [6, 7, 8, 9, 10]
     x.extend(y)
     print(x)
+    r = [1, 2, 3, 4, 5, 1, 2, 3]
+    print(r.index(3))
+    print(r.index(3, 3))
+    print(r.count(3))
+    if 5 in r:
+        print("exist")
+    if 100 in r:
+        print("exist")
+    print(r)
+    r.sort()
+    print(r)
+    r.sort(reverse=True)
+    print(r)
+    r.reverse()
+    print(r)
+    s = "My name is Mike."
+    to_split_blank = s.split(" ")
+    print(to_split_blank)
+    to_split_exclamation_mark = s.split("!!!")
+    print(to_split_exclamation_mark)
+    x = " ".join(s)
+    print(x)
+    x = "!!!".join(s)
+    print(x)
+    print(help(list))
