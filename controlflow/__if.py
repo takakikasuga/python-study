8ecf893068e91ac8dc819b4138dc419c2a399f1c
takakikasuga <taka.engineer.works@gmail.com>
2024-01-09 08:41:11 +0900

    if文

---
+++ controlflow/__if.py
@@ -0,0 +1,18 @@
+x = 10
+def get_if():
+    if x < 0:
+        print("negative")
+    elif x == 0:
+        print("zero")
+    elif x == 10:
+        print("1000000000")
+    elif x == 10:
+        print("10")
+    else:
+        print("positive")
+    a = 5
+    b = 10
+    if a > 0:
+        print("a is positive")
+        if b > 0:
+            print("b is positive")
---
+++ controlflow/main.py
@@ -0,0 +1,3 @@
+import __if
+
+__if.get_if()
