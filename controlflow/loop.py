af583ff84e66c67533d1790089b5b3f4d0c3e2df
takakikasuga <taka.engineer.works@gmail.com>
2024-01-09 09:35:39 +0900

    for else文

--- controlflow/loop.py
+++ controlflow/loop.py
@@ -47,3 +47,14 @@ def get_loop():
         if word == "Mike":
             break
         print(word)
+    for fruit in ["apple", "banana", "orange"]:
+        print(fruit)
+    else:
+        print("I ate all!")
+    for fruit in ["apple", "banana", "orange"]:
+        if fruit == "banana":
+          print("stop eating")
+          break
+        print(fruit)
+    else:
+        print("I ate all!")
