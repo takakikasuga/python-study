23ce48826be27ae2889b58a6b520ecd3e6d0f608
takakikasuga <taka.engineer.works@gmail.com>
2024-01-09 09:22:07 +0900

    whileとbreak, continue文

---
+++ controlflow/loop.py
@@ -0,0 +1,14 @@
+def get_loop():
+    count = 0
+    while count < 5:
+        print(count)
+        count += 1
+    count = 0
+    while True:
+        if count >= 5:
+            break
+        if count == 2:
+            count += 1
+            continue
+        print(count)
+        count += 1
--- controlflow/main.py
+++ controlflow/main.py
@@ -3,9 +3,11 @@ import logic
 import innot
 import falsy
 import none
+import loop
 
 __if.get_if()
 logic.get_calc_logic()
 innot.get_in_not()
 falsy.get_falsy()
 none.get_none()
+loop.get_loop()
