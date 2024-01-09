d47b2298e3c6e0538b91c838bc4ac2271812398e
takakikasuga <taka.engineer.works@gmail.com>
2024-01-09 09:33:29 +0900

    forとbreak, continue文

--- controlflow/loop.py
+++ controlflow/loop.py
@@ -26,8 +26,24 @@ def get_loop():
         count += 1
     else:
         print("done")
-    while True:
-        word = input("Enter:")
-        if word == "ok":
+    # while True:
+    #     word = input("Enter:")
+    #     if word == "ok":
+    #         break
+    #     print("next")
+    some_list = [1, 2, 3, 4, 5]
+    # i = 0
+    # while i < len(some_list):
+    #     print(some_list[i])
+    #     i += 1
+    for i in some_list:
+        print(i)
+    for s in "abcde":
+        print(s)
+
+    for word in ["My", "name", "is", "Mike"]:
+        if word == "name":
+            continue
+        if word == "Mike":
             break
-        print("next")
+        print(word)
