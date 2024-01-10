7c3449f32bbd8d52369e5ce0d8bf2c71e324023b
takakikasuga <taka.engineer.works@gmail.com>
2024-01-10 21:51:44 +0900

    位置引数のタプル化

--- function/__function.py
+++ function/__function.py
@@ -44,3 +44,10 @@ def get_function():
     print(r)
     r = test_func(100)
     print(r)
+    def say_something(word, *args):
+        print("word = ", word)
+        for arg in args:
+            print(arg)
+    say_something("Hi", "Mike", "Nance")
+    t = ("Mike", "Nancy")
+    say_something("Hi", *t)
