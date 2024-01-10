d57be46430d9231aadcd141616df58311d129a28
takakikasuga <taka.engineer.works@gmail.com>
2024-01-10 21:48:12 +0900

    デフォルト引数で気をつけること

--- function/__function.py
+++ function/__function.py
@@ -29,3 +29,18 @@ def get_function():
         print("drink = ", drink)
     menu()
     menu(entree="beef", dessert="ice", drink="beer")
+    def test_func(x, l=None):
+        if l is None:
+            l = []
+        l.append(x)
+        return l
+    y = [1, 2, 3]
+    r = test_func(100, y)
+    print(r)
+    y = [1, 2, 3]
+    r = test_func(200, y)
+    print(r)
+    r = test_func(100)
+    print(r)
+    r = test_func(100)
+    print(r)
